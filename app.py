f62ec97e1d5d3c6b2862289ca56bef9984d342bb
lucio <seanluciotolentino@gmail.com>
2016-06-04 16:43:36 -0400

    Update graphics

--- app.py
+++ app.py
@@ -5,25 +5,43 @@ import math
 import pandas as pd
 import os
 import datetime
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def default():
     return render_template('index.html')
+
+@app.route('/test')    
+@app.route('/test/<metric>')  
+def test(metric=None):
+    global sample
+    for s in sample:
+        s['color'] = s[metric+'_col']
+    return jsonify({'polygons':sample})    
     
-@app.route('/test')
-def test():
-    data = {'polygons':[{'bounds':[[40.736, -73.959], [40.737, -73.959]],
-                         'id':20}
-                         ]
-            }
-    return jsonify(data)
-    #return jsonify([{'lat'}])
-    
+def popup_text(s):
+    return """percent alive: %s<br>
+              average size: %s<br>
+              number of species: %s<br>"""%(s['aliveness'],s['average_size'],s['diversity'])
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
     # run the app
+    print 'loading the data...'
+    sample = json.load(open('data/square.json', 'r'))
+    for s in sample:
+        del s['']
+        try:
+            s['bounds'] = json.loads(s['bounds'])
+            s['size_col'] = s['dbh_col']
+            s['diversity_col'] = s['species_col']
+            s['size'] = s['average_size']
+            s['popup_text'] = popup_text(s)
+        except KeyError as e:
+            #print e, '||', s
+            continue
+    print '...done.'
     app.run(debug = True)
     #app.run(host='0.0.0.0', port=int(port))
